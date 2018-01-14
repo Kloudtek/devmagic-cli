9eb0a4801e2e66aec5e6ee83af47962800cb82e3
Yannick Menager <ymenager@kloudtek.com>
2018-01-13 20:20:24 -0800

    Imported

---
+++ README.md
@@ -0,0 +1,2 @@
+# Overview
+DevMagic is a developer's swiss army knife, allowing to easily perform complex operations with short commands
---
+++ src/dist/devmagic.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+java -cp "${DIR}/lib/*" com.kloudtek.devmagic.DevMagicCli $@
